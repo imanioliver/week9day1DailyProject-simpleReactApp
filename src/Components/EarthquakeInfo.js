dd4b1d2f182a2e0831a87eef8ce1e410ee72eb1d
Imani Oliver <isaac@ihardy.co>
2017-09-14 16:10:21 -0400

    linked all components to app.js

---
+++ src/Components/EarthquakeInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class EarthquakeInfo extends Component {
+
+    render(){
+    return (
+      <div className="earthquake-title">
+        <p> This is a list of 8 Earthquakes occuring on the morning of July 14th across the United States.</p>
+
+      </div>
+    );
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
+import './styles/App.css';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
